47ebbf07f501359ad2fee84e895b8d3fdff0b9e1
lokesh <lokesh.s@hp.com>
2014-08-06 15:17:47 +0530

    first commit

---
+++ windows-diskimage-builder/SetupComplete.cmd
@@ -0,0 +1,2 @@
+msiexec /i "%systemroot%\Setup\Scripts\CloudbaseInitSetup_Beta_x64.msi" /q /l "%systemroot%\Setup\Scripts\cloudbaseinstalllog.txt"
+del "%systemroot%\Setup\Scripts\CloudbaseInitSetup_Beta_x64.msi"
