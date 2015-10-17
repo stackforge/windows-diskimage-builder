cabff92bd906a59f3e73b8ebe6d8abd2585f8016
Monty Taylor <mordred@inaugust.com>
2015-10-17 16:05:09 -0400

    Retire stackforge/windows-diskimage-builder

--- windows-diskimage-builder/SetupComplete.cmd
+++
@@ -1,2 +0,0 @@
-msiexec /i "%systemroot%\Setup\Scripts\CloudbaseInitSetup_Beta_x64.msi" /q /l "%systemroot%\Setup\Scripts\cloudbaseinstalllog.txt"
-del "%systemroot%\Setup\Scripts\CloudbaseInitSetup_Beta_x64.msi"
